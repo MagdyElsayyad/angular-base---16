1a22399cad9c8510c29e655a5478ee753dbff870
Magdy Elsayyad <medoelsayad229@gmail.com>
2023-06-05 20:11:59 +0300

    update identity

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mena-sim'`, () => {
+  it(`should have as title 'mg-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mena-sim');
+    expect(app.title).toEqual('mg-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mena-sim app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('mg-angular app is running!');
   });
 });
